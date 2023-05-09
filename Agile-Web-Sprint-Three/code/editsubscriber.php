977d1fbbcaaf694669bfd85253a5d25cabb06ab0
Infonite2413 <98448784+Infonite2413@users.noreply.github.com>
2023-05-09 10:53:12 +0800

    Edit fix, navbar admin invis
    Edit sub now wont let user not subscriberd to anything.
    Navbar Admin item is now invisible

--- Agile-Web-Sprint-Three/code/editsubscriber.php
+++ Agile-Web-Sprint-Three/code/editsubscriber.php
@@ -10,22 +10,26 @@
 			$subscriberEmail = $_POST['subscriberEmail'];
 			$subscriberMonthly = $_POST['subscriberMonthly'];
 			$subscriberBreaking = $_POST['subscriberBreaking'];
-            $sql = "UPDATE subscriberstable
-            SET subscriberName = :subscriberName,
-            subscriberEmail = :subscriberEmail,
-            subscriberMonthly = :subscriberMonthly,
-            subscriberBreaking = :subscriberBreaking
-            WHERE subscriberEmail = '$id'";
-            $step=$db->prepare($sql);
-            $step->bindParam(':subscriberName',$subscriberName,PDO::PARAM_STR, 20);
-            $step->bindParam(':subscriberEmail',$subscriberEmail,PDO::PARAM_STR, 20);
-            $step->bindParam(':subscriberMonthly',$subscriberMonthly,PDO::PARAM_INT, 20);
-            $step->bindParam(':subscriberBreaking',$subscriberBreaking,PDO::PARAM_INT, 20);
-            if($step->execute()){
-                $_SESSION['message'] = 'Subscriber details updated as requested successfully';
-            }
-            else{
-                $_SESSION['message']  = 'Not able to add data please contact Admin ';
+            if(!isset($subscriberMonthly) && !isset($subscriberBreaking)) {
+				$_SESSION['message'] = 'Operation Invalid';
+			} else {
+                $sql = "UPDATE subscriberstable
+                SET subscriberName = :subscriberName,
+                subscriberEmail = :subscriberEmail,
+                subscriberMonthly = :subscriberMonthly,
+                subscriberBreaking = :subscriberBreaking
+                WHERE subscriberEmail = '$id'";
+                $step=$db->prepare($sql);
+                $step->bindParam(':subscriberName',$subscriberName,PDO::PARAM_STR, 20);
+                $step->bindParam(':subscriberEmail',$subscriberEmail,PDO::PARAM_STR, 20);
+                $step->bindParam(':subscriberMonthly',$subscriberMonthly,PDO::PARAM_INT, 20);
+                $step->bindParam(':subscriberBreaking',$subscriberBreaking,PDO::PARAM_INT, 20);
+                if($step->execute()){
+                    $_SESSION['message'] = 'Subscriber details updated as requested successfully';
+                }
+                else{
+                    $_SESSION['message']  = 'Not able to add data please contact Admin ';
+                }
             }
         } catch(PDOException $e){
             $_SESSION['message'] = $e->getMessage();
